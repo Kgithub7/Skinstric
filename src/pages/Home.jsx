0b908a4876f4982e30fe155bb62afe537bb0a6e1
Kobi <knjoku@hawk.illinoistech.edu>
2025-10-01 21:27:02 -0500

    Add animations to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,11 @@ import Nav from "../components/Nav";
 
 function Home() {
   return (
-    <div className="fixed h-screen">
-      <div className="absolute aspect-square h-screen -translate-x-4/5 rotate-45 overflow-hidden border-1 border-dotted border-gray-400"></div>
+    <div className="fixed top-0 left-0 h-screen w-screen [&:has(.left-arrow:hover)_.right-arrow]:invisible [&:has(.left-arrow:hover)_.right-arrow]:opacity-0 [&:has(.left-arrow:hover)_.right-square]:opacity-0 [&:has(.right-arrow:hover)_.left-arrow]:invisible [&:has(.right-arrow:hover)_.left-arrow]:opacity-0 [&:has(.right-arrow:hover)_.left-square]:opacity-0">
+      <div className="left-square absolute aspect-square h-screen -translate-x-4/5 rotate-45 overflow-hidden border-1 border-dotted border-gray-400 transition-all duration-900 ease-in-out"></div>
       <Link
         to={"/"}
-        className="absolute top-1/2 right-17/20 z-1 -translate-y-1/2 text-[14px]"
+        className="left-arrow absolute top-1/2 right-17/20 z-1 -translate-y-1/2 text-[14px] transition-all duration-900 ease-in-out"
       >
         <div className="group flex items-center gap-x-5">
           <div className="rotate-45 items-center border-1 p-0 transition-transform duration-300 ease-in-out group-hover:scale-110">
@@ -18,10 +18,10 @@ function Home() {
           <span>DISCOVER A.I.</span>
         </div>
       </Link>
-      <div className="absolute right-0 aspect-square h-screen translate-x-4/5 rotate-45 overflow-hidden border-1 border-dotted border-gray-400"></div>
+      <div className="right-square absolute right-0 aspect-square h-screen translate-x-4/5 rotate-45 overflow-hidden border-1 border-dotted border-gray-400 transition-all duration-900 ease-in-out"></div>
       <Link
         to={"/testing"}
-        className="peer absolute top-1/2 left-17/20 z-1 -translate-y-1/2 text-[14px]"
+        className="right-arrow absolute top-1/2 left-17/20 z-1 -translate-y-1/2 text-[14px] transition-all duration-900 ease-in-out"
       >
         <div className="group flex items-center gap-x-5">
           <span>TAKE TEST</span>
@@ -32,9 +32,9 @@ function Home() {
           </div>
         </div>
       </Link>
-      <div className="absolute top-1/2 left-1/2 container flex -translate-1/2 animate-fade flex-col items-center text-[100px] leading-none transition-all duration-700 ease-in-out animate-duration-2000 animate-ease-in-out peer-hover:left-0 peer-hover:-translate-x-1/4">
+      <div className="absolute top-1/2 left-1/2 -translate-1/2 animate-fade text-center text-[100px] leading-none transition-all duration-900 ease-in-out animate-duration-2000 animate-ease-in-out [.left-arrow:hover~&]:left-3/4 [.left-arrow:hover~&>p]:translate-x-1/5 [.right-arrow:hover~&]:left-1/4 [.right-arrow:hover~&>p]:-translate-x-1/6">
         <h1>Sophisticated</h1>
-        <span>skincare</span>
+        <p className="transition-all duration-900 ease-in-out">skincare</p>
       </div>
       <div className="absolute bottom-12 left-2 text-[14px]">
         <p>
@@ -46,5 +46,5 @@ function Home() {
     </div>
   );
 }
-// ▶
+
 export default Home;
